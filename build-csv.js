6d35e0b6704b713113e3acb028257746e3cd89bf
Levana Xue <levana.xue@2gotech.com>
2017-11-15 11:31:20 +0800

    添加个性化语言 合并支持

--- build-csv.js
+++ build-csv.js
@@ -3,46 +3,56 @@ const fs = require('fs');
 const jsonfile = require('jsonfile');
 const YAML = require('yamljs');
 
-let path = './json-file/';
-let csvFile = './build/result-csv.csv';
-
 let config = YAML.load('./_config.yml');
 
-path = config.json_convert_json.json_file_path;
-csvFile = './build/'+ config.json_convert_json.csv_name;
+let jsonFilePath = config.json_convert_csv.json_path;
+let csvFilePath = config.json_convert_csv.csv_path;
+if (util.isEmpty(jsonFilePath) || util.isEmpty(csvFilePath)) {
+	throw new Error('Please check config ');
+}
 
-let jsonDir = fs.readdirSync(path);
+let jsonDir = fs.readdirSync(jsonFilePath);
 
 let contentArray = [];
 let languageObjects = [];
 let colTitle = [];
-for(let i = 0, length = jsonDir.length; i < length; i++){
-	if(!util.isEmpty(jsonDir[i]) 
-		&& new RegExp(/.json$/).test(jsonDir[i])){
-		languageObjects.push(jsonfile.readFileSync(path+ jsonDir[i]));
+for (let i = 0, length = jsonDir.length; i < length; i++) {
+	if (!util.isEmpty(jsonDir[i]) && new RegExp(/.json$/).test(jsonDir[i])) {
+		let languageObj = jsonfile.readFileSync(jsonFilePath + jsonDir[i]);
+
+		let individuationJsonPath = jsonFilePath+'individuation/' + jsonDir[i];
+		if(fs.existsSync(individuationJsonPath)){
+			let indevLanguageObj = jsonfile.readFileSync(individuationJsonPath);
+			languageObj = Object.assign(languageObj, indevLanguageObj);
+		}
+
+		languageObjects.push(languageObj);
+
 		colTitle.push(jsonDir[i].match(/[-_a-zA-Z0-9]+/)[0]);
 	}
 }
-contentArray[0] = 'key, '+ colTitle.join(',');
+
+contentArray[0] = 'key, ' + colTitle.join(',');
 contentArray = contentArray.concat(util.processMultipleLanguage(languageObjects));
-try{
-	fs.unlinkSync(csvFile);
-}catch(e){
+
+try {
+	fs.unlinkSync(csvFilePath);
+} catch (e) {
 	console.log('file is not exist.');
 }
 
-fs.open(csvFile, 'w', (err, fd) => {
+fs.open(csvFilePath, 'w', (err, fd) => {
 	let contentBuffer = Buffer.concat([
-			new Buffer('\xEF\xBB\xBF', 'binary'),
-	    	new Buffer(contentArray.join('\n'))
-		]);
-	fs.writeFile(csvFile, contentBuffer ,{
-		encoding : 'utf8'
-	}, function(err){
-		if(err){
+		new Buffer('\xEF\xBB\xBF', 'binary'),
+		new Buffer(contentArray.join('\n'))
+	]);
+	fs.writeFile(csvFilePath, contentBuffer, {
+		encoding: 'utf8'
+	}, function(err) {
+		if (err) {
 			console.log(err);
-		}else{
-			console.log('build complete. please go : ' + csvFile);
+		} else {
+			console.log('build complete. please go : ' + csvFilePath);
 		}
 	});
 })
--- readme.md
+++ readme.md
@@ -1,16 +1,19 @@
+
+## 生成csv 文件  
 ###  config:
-json_convert_json  
--  json_file_path ->json文件夹路径 
--  csv_name  ->生成csv文件名
+json_convert_csv    
+-  json_path ->json文件夹路径 ./json-file/
+-  csv_path  ->生成csv文件路徑 ./result.csv  
 
 ### 使用:
 
-1) 将json文件 放置 json-file 文件夹下
-2) 执行以下命令：
+1) 将json文件 放置 {json_path} 文件夹下
+2) 如果有个性化语言文件，请放置 {json_path}/individuation/ 文件夹下，注意：同类语言文件名必需相同  
+3) 执行以下命令：
 ```
 $npm run build-csv  
 ```
-3) csv文件在build 文件夹下
+3) csv文件在 {csv_path}
 
 ---
 
